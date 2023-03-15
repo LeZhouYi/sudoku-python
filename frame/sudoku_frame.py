27d51d002a959d919747df9739189333cf423dcb
mhhu <mhhu@liangzhicn.com>
2023-03-15 21:15:07 +0800

    add chess

--- frame/sudoku_frame.py
+++ frame/sudoku_frame.py
@@ -40,6 +40,20 @@ class SudokuFrame(object):
         self.mainWindow.bind("<KeyPress-8>",eventAdaptor(clickNumber,frame=self,sudoku=self.sudoku,number=8))#按8
         self.mainWindow.bind("<KeyPress-9>",eventAdaptor(clickNumber,frame=self,sudoku=self.sudoku,number=9))#按9
 
+        chess = [0,0,2,0,9,0,7,0,0,
+            0,0,0,0,6,2,0,0,9,
+            0,6,0,0,0,0,0,0,3,
+            0,0,0,0,0,0,0,0,8,
+            8,9,5,0,2,0,6,1,4,
+            3,0,0,0,0,0,0,0,0,
+            7,0,0,0,0,0,0,5,0,
+            5,0,0,9,3,0,0,0,0,
+            0,0,1,0,8,0,3,0,0]
+        for i in range(81):
+            if chess[i]!=0:
+                print(chess[i])
+                self.sudoku.setLatticeDisplay(i,chess[i])
+
         self.mainWindow.mainloop() #显示窗口
 
 ##----------------------------InterFace------------------------------------##
